264c82d9818b29e8dfe7490655b1903374228dc4
eddiesands98gmail <eddiesands98@gmail.com>
2023-07-15 18:48:24 -0500

    using hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import Welcome from "./components/welcome";
 import IntakeForm from "./components/form";
 import ThankYou from "./components/thankyou";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/further_interview/",
-    element: <Welcome></Welcome>,
-  },
-  {
-    path: "/further_interview/form/",
-    element: <IntakeForm></IntakeForm>,
-  },
-  {
-    path: "/further_interview/thankyou/",
-    element: <ThankYou></ThankYou>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/further_interview/",
+//     element: <Welcome></Welcome>,
+//   },
+//   {
+//     path: "/further_interview/form/",
+//     element: <IntakeForm></IntakeForm>,
+//   },
+//   {
+//     path: "/further_interview/thankyou/",
+//     element: <ThankYou></ThankYou>,
+//   },
+// ]);
 
 function App() {
   const handleNextScreen = () => {
     return 0;
   };
 
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={<div> Not Found or You do not have permission.</div>}
+        />
+        <Route exact path="/" element={<Welcome />} />
+        <Route exact path="/thankyou" element={<ThankYou />} />
+        <Route exact path="/form" element={<IntakeForm />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
